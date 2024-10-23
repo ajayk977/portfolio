ea2dd186950e151cacf6e0dfdd878d6e1c635ce0
ajay kumar <ajayk970349@gmail.com>
2024-10-23 16:57:30 +0530

    deployment ready code3

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,3 @@
-// app/api/contact/route.js
 import dbConnect from '@/lib/dbConnect';
 import Message from '@/model/contact.model';
 
@@ -7,20 +6,19 @@ export async function POST(req: Request) {
 
     try {
         const { name, email, message } = await req.json();
-        console.log('Received message:', name, email, message);
 
         // Validate input
         if (!name || !email || !message) {
             return new Response(JSON.stringify({ success: false, message: 'All fields are required.' }), { status: 400 });
         }
 
-        // Create and save new message
+        // Create and save new message in a single operation
         const newMessage = new Message({ name, email, message });
         await newMessage.save();
-        console.log('New message saved:', newMessage);
 
         return new Response(JSON.stringify({ success: true, message: 'Message sent!' }), { status: 200 });
     } catch (error) {
-        return new Response(JSON.stringify({ success: false, message: 'An error occurred while sending your message. Please try again later.' }), { status: 500 });
+        console.error('Error:', error); // Log error for debugging
+        return new Response(JSON.stringify({ success: false, message: 'An error occurred while sending your message.' }), { status: 500 });
     }
 }
